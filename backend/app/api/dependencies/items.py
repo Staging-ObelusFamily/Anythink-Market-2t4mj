d7b0ad9301b364e84ff99adf70dd11394e45db19
nivrichter <nivrichter@gmail.com>
2022-06-26 12:57:21 +0300

    solved

--- backend/app/api/dependencies/items.py
+++ backend/app/api/dependencies/items.py
@@ -22,10 +22,12 @@ def get_items_filters(
     tag: Optional[str] = None,
     seller: Optional[str] = None,
     favorited: Optional[str] = None,
+    title: Optional[str] = None,
     limit: int = Query(DEFAULT_ITEMS_LIMIT, ge=1),
     offset: int = Query(DEFAULT_ITEMS_OFFSET, ge=0),
 ) -> ItemsFilters:
     return ItemsFilters(
+        title=title,
         tag=tag,
         seller=seller,
         favorited=favorited,
